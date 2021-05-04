2cc99e0dc384256a4321e32ed4c09fe116acf225
vjspranav <pranavasri@live.in>
2021-05-05 03:50:06 +0530

    Create a base for website

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Webpage for StagOS Maintainer Application Form
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div
+      style={{
+        position: "center",
+        marginLeft: "10%",
+        marginRight: "10%",
+        marginTop: "10%",
+      }}
+    >
+      <h1 style={{ color: "#4285f4" }}>StagOS Maintainer Form</h1>
+    </div>
+  );
 }
 
 export default App;
